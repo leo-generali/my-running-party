7fe76b30ba21b3adebd91d361166b582479ab890
Leo Generali <leogen17@gmail.com>
2020-03-26 21:05:06 -0400

    Initial commit

---
+++ src/_data/css.js
@@ -0,0 +1,27 @@
+const postcss = require("postcss");
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/pages/*.njk"],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+const path = require("path");
+const fs = require("fs");
+
+const postCssConfig = {
+  development: [tailwindcss],
+  production: [tailwindcss, autoprefixer, purgecss, cssnano]
+};
+
+module.exports = async () => {
+  const stylesheet = await fs.readFileSync(
+    path.join(__dirname, "..", "assets", "index.css"),
+    "utf8"
+  );
+
+  const css = await postcss(postCssConfig[process.env.ENV]).process(stylesheet);
+
+  return css;
+};
