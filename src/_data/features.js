2a641778d68f87d95da33c01c51644a1900e9b40
Leo Generali <leogen17@gmail.com>
2020-03-31 15:09:22 -0400

    Add some basic styles to home

--- src/_data/features.js
+++ src/_data/features.js
@@ -4,13 +4,15 @@ module.exports = [
     shortName: "Plans",
     url: "/plans/",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit eleifend scelerisque. Vestibulum et magna sed ante scelerisque scelerisque. Duis fringilla viverra odio at condimentum. Phasellus volutpat tristique consectetur."
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit eleifend scelerisque. Vestibulum et magna sed ante scelerisque scelerisque. Duis fringilla viverra odio at condimentum. Phasellus volutpat tristique consectetur.",
+    emoji: "📆"
   },
   {
     name: "Pace Calculator",
     shortName: "Calc",
     url: "/pace-calculator/",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit eleifend scelerisque. Vestibulum et magna sed ante scelerisque scelerisque. Duis fringilla viverra odio at condimentum. Phasellus volutpat tristique consectetur."
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit eleifend scelerisque. Vestibulum et magna sed ante scelerisque scelerisque. Duis fringilla viverra odio at condimentum. Phasellus volutpat tristique consectetur.",
+    emoji: "🏃"
   }
 ];
